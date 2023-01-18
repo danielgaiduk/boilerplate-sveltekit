96e904e8b91f06002875804a962c1ee567604bd6
Daniel Gaiduk <daniel@gaiduk.dev>
2023-01-18 10:12:34 +0100

    Fix duplicate server calls

--- src/lib/config/routing.ts
+++ src/lib/config/routing.ts
@@ -1,4 +1,4 @@
 // Path exceptions are paths that should not be redirected to the language prefix
-const PATH_EXCEPTIONS = ['api', 'sitemap.xml', 'robots.txt']
+const PATH_EXCEPTIONS = ['api', 'sitemap.xml', 'robots.txt', 'favicon.png', 'manifest.json']
 
 export { PATH_EXCEPTIONS }
